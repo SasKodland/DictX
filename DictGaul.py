a9365549e16dc0fc4554938c98a45b9b47f3ffdb
SasKodland <139468320+SasKodland@users.noreply.github.com>
2023-07-13 20:24:17 +0700

    Create DictGaul.py
    :D projectss

---
+++ DictGaul.py
@@ -0,0 +1,14 @@
+meme_dict = {
+            "CRINGE": "Sesuatu yang sangat aneh atau memalukan",
+            "LOL": "Tanggapan umum terhadap sesuatu yang lucu",
+            "BTW": "By The way",
+            }
+            
+
+
+word = input("Ketik kata yang tidak Kamu mengerti (gunakan huruf kapital semua!): ")
+
+if word in meme_dict.keys():
+    print(meme_dict[word])
+else:
+    print("kata tidak ada dalam kamus :(")
